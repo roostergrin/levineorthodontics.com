aba6b24bfc619e03782a72321227ee4e373d6441
GreenWebMedia <37212563+GreenWebMedia@users.noreply.github.com>
2023-08-11 16:39:53 +0530

    Update index.php
    Google tag

--- dist/index.php
+++ dist/index.php
@@ -4,6 +4,14 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">   
     <?php wp_head(); ?>
+<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-C4GG7BW525"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date()); 
+		gtag('config', 'G-C4GG7BW525');
+	</script>
 </head>
 <body <?php echo body_class();  ?>>
   <div id="app"></div>
