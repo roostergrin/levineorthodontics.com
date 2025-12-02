d462bc5cc211808c2975a2985f95aa6a0a1984c0
jmurphy1196 <jmurphy.1196@gmail.com>
2025-12-02 11:59:21 -0800

    openchair adjustment

--- dist/index.php
+++ dist/index.php
@@ -97,9 +97,6 @@ token: "eyJhbGciOiJIUzI1NiJ9.eyJvcmlnaW4iOiJsZXZpbmVvcnRob2RvbnRpY3MuY29tIiwicHJ
 
 hideTooltipHeader: true,
 
-adultPatientsOnly: true,
-
-minorPatientsOnly: true,
 
 floatDownLocsWithoutBookings: true,
 
